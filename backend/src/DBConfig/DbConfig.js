cde54d6fdaf238183d50bd4eb33eb795d59ed760
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-28 22:41:21 +0530

    Writing A Code In Update User

--- backend/src/DBConfig/DbConfig.js
+++ backend/src/DBConfig/DbConfig.js
@@ -11,3 +11,40 @@ async function ConnectDB() {
 module.exports = ConnectDB;
 
 
+/*
+❌ MongoDB Connection Error: MongooseServerSelectionError: connect ECONNREFUSED 127.0.0.1:27017
+    at _handleConnectionErrors (D:\KUMAR\Codings\Projects\MERN\Todo Using MERN\backend\node_modules\mongoose\lib\connection.js:1165:11)
+    at NativeConnection.openUri (D:\KUMAR\Codings\Projects\MERN\Todo Using MERN\backend\node_modules\mongoose\lib\connection.js:1096:11)
+    at runNextTicks (node:internal/process/task_queues:60:5)
+    at listOnTimeout (node:internal/timers:540:9)
+    at process.processTimers (node:internal/timers:514:7) {
+  errorLabelSet: Set(0) {},
+  reason: TopologyDescription {
+    type: 'Unknown',
+    servers: Map(1) { '127.0.0.1:27017' => [ServerDescription] },
+    stale: false,
+    compatible: true,
+    heartbeatFrequencyMS: 10000,
+    localThresholdMS: 15,
+    setName: null,
+    maxElectionId: null,
+    maxSetVersion: null,
+    commonWireVersion: 0,
+    logicalSessionTimeoutMinutes: null
+  },
+  code: undefined,
+  cause: TopologyDescription {
+    type: 'Unknown',
+    servers: Map(1) { '127.0.0.1:27017' => [ServerDescription] },
+    stale: false,
+    compatible: true,
+    heartbeatFrequencyMS: 10000,
+    localThresholdMS: 15,
+    setName: null,
+    maxElectionId: null,
+    maxSetVersion: null,
+    commonWireVersion: 0,
+    logicalSessionTimeoutMinutes: null
+  }
+}
+  */
--- frontend/src/Pages/UpdateUser.jsx
+++ frontend/src/Pages/UpdateUser.jsx
@@ -1,9 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 
 function UpdateUser() {
+
+    // const [Data, setData] = useState({
+    //     name,
+
+    // })
+
+    function UpdateUserData() {
+
+    }
+
+    function HandleUpdateUser(e) {
+        console.log(e.target.value)
+    }
+
     return (
         <>
-            <h1>VP</h1>
+            <div id="UpdateUser">
+
+                <form onSubmit={HandleUpdateUser}>
+
+                    <div>
+                        <label htmlFor="name">Name :</label>
+                        <input
+                            type="text"
+                            placeholder="Update Name"
+                            required
+                            onChange={UpdateUser}
+                        />
+                    </div>
+
+                </form>
+
+            </div>
         </>
     )
 }
