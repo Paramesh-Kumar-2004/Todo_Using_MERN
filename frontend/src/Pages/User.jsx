74cdc67eeefda114c778179f81dc6e7694c1da3a
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-30 23:32:20 +0530

    Working On Frontend Update User Page - Styles

--- frontend/src/Pages/User.jsx
+++ frontend/src/Pages/User.jsx
@@ -12,7 +12,7 @@ import UpdateUser from './UpdateUser'
 function User() {
 
   const [data, setdata] = useState([]);
-  const [updateUserPopup, setUpdateUserPopup] = useState(false)
+  const [updateUserPopup, setUpdateUserPopup] = useState(true)
   const [Userid, setUserid] = useState(null)
 
 
