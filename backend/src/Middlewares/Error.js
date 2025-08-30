2905658056c88fa5689f9d0e92c62d67eed7fad8
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-08-30 22:46:51 +0530

    Working On Backend UpdateChar Controller

--- backend/src/Middlewares/Error.js
+++ backend/src/Middlewares/Error.js
@@ -3,11 +3,11 @@
 module.exports = (err, req, res, next) => {
     err.statuscode = err.statuscode || 500
 
+
     res.status(err.statuscode).json({
         "success": false,
         "message": err.message,
         "stack": err.stack,
     })
 }
-
 
